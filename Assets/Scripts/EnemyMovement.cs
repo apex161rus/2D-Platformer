6963b63f7686e977f06a2799db6e74cd5a5222c2
Владислав161 <64612715+apex161rus@users.noreply.github.com>
2022-07-05 06:01:59 +0300

    test 2

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,28 +7,50 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private Transform[] _poins;
     [SerializeField] private Rigidbody2D _rigidbody2D;
 
-    //private Enemy _enemy;
+    private Enemy _enemy;
+
     private int _cerentePoint;
 
     private void Start()
     {
-
+        _enemy = GetComponent<Enemy>();
     }
 
     private void Update()
     {
-        Transform target = _poins[_cerentePoint];
-        transform.position = Vector2.MoveTowards(transform.position, target.position, 1.5f * Time.deltaTime);
-
-        if (transform.position == target.position)
+        if (_enemy.IsKill != true)
         {
-            _cerentePoint++;
-            if (_cerentePoint >= _poins.Length)
+            Transform target = _poins[_cerentePoint];
+            if (_cerentePoint == 0)
             {
-                _cerentePoint = 0;
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            Debug.Log(_cerentePoint);
+            transform.position = Vector2.MoveTowards(transform.position, target.position, 1.5f * Time.deltaTime);
+
+            if (transform.position == target.position)
+            {
+                _cerentePoint++;
+                if (_cerentePoint >= _poins.Length)
+                {
+                    _cerentePoint = 0;
+                }
             }
         }
+        //Transform target = _poins[_cerentePoint];
+        //transform.position = Vector2.MoveTowards(transform.position, target.position, 1.5f * Time.deltaTime);
 
-        //Vector2.MoveTowards(_rigidbody2D.position, target.position, 1f * Time.deltaTime);
+        //if (transform.position == target.position)
+        //{
+        //    _cerentePoint++;
+        //    if (_cerentePoint >= _poins.Length)
+        //    {
+        //        _cerentePoint = 0;
+        //    }
+        //}
     }
 }
