f094761c2297005c21fb8dd9e28e8c679ca00974
Владислав161 <64612715+apex161rus@users.noreply.github.com>
2022-07-05 06:23:39 +0300

    test 2

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
+
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private Transform[] _poins;
-    [SerializeField] private Rigidbody2D _rigidbody2D;
 
     private Enemy _enemy;
-
     private int _cerentePoint;
 
     private void Start()
@@ -21,6 +21,7 @@ public class EnemyMovement : MonoBehaviour
         if (_enemy.IsKill != true)
         {
             Transform target = _poins[_cerentePoint];
+
             if (_cerentePoint == 0)
             {
                 transform.eulerAngles = new Vector3(0, 180, 0);
@@ -29,28 +30,18 @@ public class EnemyMovement : MonoBehaviour
             {
                 transform.eulerAngles = new Vector3(0, 0, 0);
             }
-            Debug.Log(_cerentePoint);
+
             transform.position = Vector2.MoveTowards(transform.position, target.position, 1.5f * Time.deltaTime);
 
             if (transform.position == target.position)
             {
                 _cerentePoint++;
+
                 if (_cerentePoint >= _poins.Length)
                 {
                     _cerentePoint = 0;
                 }
             }
         }
-        //Transform target = _poins[_cerentePoint];
-        //transform.position = Vector2.MoveTowards(transform.position, target.position, 1.5f * Time.deltaTime);
-
-        //if (transform.position == target.position)
-        //{
-        //    _cerentePoint++;
-        //    if (_cerentePoint >= _poins.Length)
-        //    {
-        //        _cerentePoint = 0;
-        //    }
-        //}
     }
 }
